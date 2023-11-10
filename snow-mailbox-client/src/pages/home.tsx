e068648283e50e56b7a4743743265ab68217f46a
YOUJIN LEE <suddenlydisappear200@gmail.com>
2023-11-10 17:29:13 +0000

    feat: user set mailbox style

--- snow-mailbox-client/src/pages/home.tsx
+++ snow-mailbox-client/src/pages/home.tsx
@@ -32,9 +32,19 @@ const MailboxWrapper = styled.div`
   width: 100%;
   height: 70%;
   align-items: center;
+  position: relative;
   justify-content: space-between;
   flex-grow: 1;
   margin-top: 30px;
+`;
+
+const DecorationImg = styled.img`
+  width: 70px;
+  position: absolute;
+  top: 140px;
+  left: 54%;
+  transform: translateX(-50%);
+  z-index: 999;
 `;
 
 const Dday = styled.div`
@@ -48,6 +58,35 @@ const Dday = styled.div`
     margin-right: 25px;
   }
 `;
+
+const MailboxSvg = ({ color }: any) => {
+  return (
+    <svg
+      width="290"
+      height="334"
+      viewBox="0 0 290 334"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M71.5 181.096L40.5 1.09644H250.5C271.5 1.09641 289.5 29.5964 289.5 61.5964V173.074L71.5 181.096Z"
+        fill={color}
+      />
+      <path
+        d="M71.5 181.096L1 168.096L2 44.5963C2.5 24.0963 14 -3.40369 42 1.59629C60.6242 4.92202 72 32.5963 71.5 44.5963V181.096Z"
+        fill={color}
+        stroke="#4D4D4D"
+        stroke-opacity="0.16"
+      />
+      <path d="M138 334V155H165V334H138Z" fill={color} />
+      <path
+        d="M10.4238 54.1357L10.5819 44.137L56.5762 44.8644L56.418 54.8631L10.4238 54.1357Z"
+        fill="#676767"
+        fill-opacity="0.23"
+      />
+    </svg>
+  );
+};
 
 const Home = () => {
   const initialTutorialState = {
@@ -98,7 +137,16 @@ const Home = () => {
           <Dday>
             <img src={calendar} /> D - 31
           </Dday>
-          <Mailbox onClick={() => navigate("/letters")} src={mailbox} />
+          <DecorationImg
+            src={require(`../assets/decorations/${data.mailbox_decorations}.png`)}
+          />
+          <MailboxSvg
+            onClick={() => navigate("/letters")}
+            className="name"
+            color={data.mailbox_color}
+          />
+
+          {/* <Mailbox  src={mailbox} /> */}
           <ButtonWrapper>
             <Button onClick={() => navigate("/home")} name="Share my mailbox" />
           </ButtonWrapper>
